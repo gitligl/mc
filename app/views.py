21fefc1b5564cc45df6df2ef9594d0894ad162a2
ligl <ligl@me.com>
2019-08-01 22:04:01 +0800

    最简单的登录操作

--- app/views.py
+++ app/views.py
@@ -1,84 +1,21 @@
-from app import app, lm, oid
-from flask import render_template, flash, redirect, session, url_for, request, g
-from flask_login import login_user, logout_user, current_user, login_required
-from app.forms import LoginForm
-from app.models import User, db
+from app import app
+from flask import render_template, request
 
 
-@app.route('/')
-@app.route('/index')
-@login_required
-def index():
-    user = g.user  # {'username': 'ligl', 'email': 'ligl@123.com'}
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'A terrible day!'
-        },
-        {
-            'author': {'username': 'Susan', 'email': 'susan@123.com'},
-            'body': 'I am a beautiful girl...'
-        }
-    ]
-    return render_template('index.html',
-                           user=user,
-                           posts=posts)
-
-
-@app.route('/login', methods=['GET', 'POST'])
-@oid.loginhandler
-def login():
-    if g.user is not None and g.user.is_authenticated:
-        return redirect(url_for('index'))
-    form = LoginForm()  #
-    if form.validate_on_submit():
-        # flash('Login requested for OpenID="' + form.openid.data + '", remember_me=' + str(form.remember_me.data))
-        # return redirect('/home')
-        session['remember_me'] = form.remember_me.data
-        return oid.try_login(form.openid.data, ask_for=['nickname', 'email'])
-    return render_template('login.html',
-                           title='Sign In',
-                           form=form,
-                           providers=app.config['OPENID_PROVIDERS'])
-
-
-@oid.after_login
-def after_login(resp):
-    if resp.email is None or resp.email == "":
-        flash('Invalid login. Please try again.')
-        return redirect(url_for('login'))
-    user = User.query.filter_by(email=resp.email).first()
-    if user is None:
-        username = resp.username
-        if username is None or username == "":
-            username = resp.email.split('@')[0]
-        user = User(username=username, email=resp.email)
-        db.session.add(user)
-        db.session.commit()
-    remember_me = False
-    if 'remember_me' in session:
-        remember_me = session['remember_me']
-        session.pop('remember_me', None)
-    login_user(user, remember=remember_me)
-    return redirect(request.args.get('next') or url_for('home'))
-
-
-@app.before_request
-def before_request():
-    g.user = current_user
-
-
-@app.route('/logout')
-def logout():
-    logout_user()
-    return redirect(url_for('index'))
-
-
-@app.route('/home')
+@app.route('/', methods=['GET', 'POST'])
 def home():
     return render_template('home.html')
 
 
-@lm.user_loader
-def load_user(id):
-    return User.query.get(int(id))
+@app.route('/signin', methods=['GET'])
+def signin_form():
+    return render_template('form.html')
+
+
+@app.route('/signin', methods=['POST'])
+def signin():
+    username = request.form['username']
+    password = request.form['password']
+    if username=='admin' and password=='password':
+        return render_template('login_ok.html', username=username)
+    return render_template('form.html', message='Bad username or password', username=username)
